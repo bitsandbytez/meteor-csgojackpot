d9fab78a6d926a545fa36e458b380b2bd3b337dd
Mike Bobadilla <mike.c.bobadilla@gmail.com>
2016-05-15 11:37:58 -0700

    basic working frontend

---
+++ client/default.js
@@ -0,0 +1,15 @@
+import { Template } from 'meteor/templating';
+
+Template.default.onRendered(function(){
+  // $(".dial").knob({
+  //     'min': 0,
+  //     'max': 50,
+  //     'fgColor': '#b71c1c',
+  //     'format' : function (value) {
+  //    return value + '/50';
+  // },
+  //     'draw': function() {
+  //       $(this.i).css('font-size', '36pt');
+  //     }
+  //   });
+})
